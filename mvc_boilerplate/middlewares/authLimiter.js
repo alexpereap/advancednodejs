76af2c83bd86e8868da73f86edc1023f539f2ad7
Alex Perea <ing.alexperea@gmail.com>
2024-10-28 18:21:37 -0500

    refactored rate limiter

--- mvc_boilerplate/middlewares/authLimiter.js
+++ mvc_boilerplate/middlewares/authLimiter.js
@@ -1,14 +1,13 @@
 const { RateLimiterMongo } = require('rate-limiter-flexible');
 const mongoose = require('mongoose');
-const ApiError = require('./../utils/ApiError');
+const ApiError = require('../utils/ApiError');
 const httpStatus = require('http-statuses');
+const config = require("../config/config")
  
-const maxAttemptsPerDay = 100;
-const maxAttemptsByIpUsername = 10;
  
 const emailIpBruteLimiter = new RateLimiterMongo({
     storeClient:mongoose.connection,
-    points:maxAttemptsByIpUsername,
+    points:config.rateLimiter.maxAttemptsByIpUsername,
     duration:60*10,
     blockDuration:60*60*24,
     dbName:"blog_app"
@@ -16,32 +15,43 @@ const emailIpBruteLimiter = new RateLimiterMongo({
  
 const slowerBruteLimiter = new RateLimiterMongo({
     storeClient:mongoose.connection,
-    points:maxAttemptsPerDay,
+    points:config.rateLimiter.maxAttemptsPerDay,
     duration:60*60*24,
     blockDuration:60*60*24,
     dbName:"blog_app"
 })
  
+const emailBruteLimiter = new RateLimiterMongo({
+  storeClient:mongoose.connection,
+  points:config.rateLimiter.maxAttemptsPerEmail,
+  duration:60*60*24,
+  blockDuration:60*60*24,
+  dbName:"blog_app"
+})
+ 
 const authLimiter = async (req, res, next) => {
     const ipAddr = req.connection.remoteAddress;
-    const emailIpKey = `${req.body.email}_${ipAddr}`;
-    const [slowerBruteRes, emailIpRes] = await Promise.all([
+    const email = req.body.email
+    const emailIpKey = `${email}_${ipAddr}`;
+    const [slowerBruteRes, emailIpRes,emailRes] = await Promise.all([
       slowerBruteLimiter.get(ipAddr),
       emailIpBruteLimiter.get(emailIpKey),
- 
+      emailBruteLimiter.get(email)
     ]);
     let retrySeconds = 0;
     if (
       slowerBruteRes &&
-      slowerBruteRes.consumedPoints >= maxAttemptsPerDay
+      slowerBruteRes.consumedPoints >= config.rateLimiter.maxAttemptsPerDay
     ) {
       retrySeconds = Math.floor(slowerBruteRes.msBeforeNext / 1000) || 1;
     } else if (
       emailIpRes &&
-      emailIpRes.consumedPoints >= maxAttemptsByIpUsername
+      emailIpRes.consumedPoints >= config.rateLimiter.maxAttemptsByIpUsername
     ) {
       retrySeconds = Math.floor(emailIpRes.msBeforeNext / 1000) || 1;
-    } 
+    } else if(emailRes&& emailRes.consumedPoints>= config.rateLimiter.maxAttemptsPerEmail){
+      retrySeconds = Math.floor(emailRes.msBeforeNext / 1000) || 1;
+    }
   
     if (retrySeconds > 0) {
       res.set('Retry-After', String(retrySeconds));
@@ -53,8 +63,10 @@ const authLimiter = async (req, res, next) => {
     next();
   };
  
+ 
   module.exports = {
     emailIpBruteLimiter,
     slowerBruteLimiter,
+    emailBruteLimiter,
     authLimiter,
   };
--- mvc_boilerplate/services/auth.service.js
+++ mvc_boilerplate/services/auth.service.js
@@ -6,14 +6,14 @@ const logger = require('./../config/logger');
 const { tokenTypes } = require('./../config/tokens')
 const { RateLimiterMongo } = require('rate-limiter-flexible');
 const mongoose = require('mongoose');
+const config = require("../config/config")
  
 const login = async (email, password,ipAddr) => {
-  const maxAttemptsPerDay = 100;
-  const maxAttemptsByIpUsername = 10;
-   
+ 
+ 
   const emailIpBruteLimiter = new RateLimiterMongo({
       storeClient:mongoose.connection,
-      points:maxAttemptsByIpUsername,
+      points:config.rateLimiter.maxAttemptsByIpUsername,
       duration:60*10,
       blockDuration:60*60*24,
       dbName:"blog_app"
@@ -21,42 +21,52 @@ const login = async (email, password,ipAddr) => {
    
   const slowerBruteLimiter = new RateLimiterMongo({
       storeClient:mongoose.connection,
-      points:maxAttemptsPerDay,
+      points:config.rateLimiter.maxAttemptsPerDay,
       duration:60*60*24,
       blockDuration:60*60*24,
       dbName:"blog_app"
   })
-    
-  const promises = [slowerBruteLimiter.consume(ipAddr)];
-  const user = await userService.getUserByEmail(email);
    
-  if (!user || !(await user.isPasswordMatch(password))) {
-    user && promises.push(emailIpBruteLimiter.consume(`${email}_${ipAddr}`))
-    await Promise.all(promises)
-    throw new ApiError(httpStatus.UNAUTHORIZED.code, 'Incorrect email or password');
-  }
-  return user;
-  };
-
-const refreshAuthToken = async (refreshToken) => {
-  try {
-    const refreshTokenDoc = await tokenService.verifyToken(
-      refreshToken,
-      tokenTypes.REFRESH
-    );
-    const user = await userService.getUserById(refreshTokenDoc.user);
-    if (!user) {
-      throw new Error();
+  const emailBruteLimiter = new RateLimiterMongo({
+    storeClient:mongoose.connection,
+    points:config.rateLimiter.maxAttemptsPerEmail,
+    duration:60*60*24,
+    blockDuration:60*60*24,
+    dbName:"blog_app"
+  })
+   
+    const promises = [slowerBruteLimiter.consume(ipAddr)]
+    const user = await userService.getUserByEmail(email);
+   
+    if (!user || !(await user.isPasswordMatch(password))) {
+      user && promises.push([emailIpBruteLimiter.consume(`${email}_${ipAddr}`),emailBruteLimiter.consume(email)])
+      await Promise.all(promises)
+      throw new ApiError(httpStatus.UNAUTHORIZED.code, 'Incorrect email or password');
     }
-    await refreshTokenDoc.deleteOne();
-    return tokenService.generateAuthTokens(user.id);
-  } catch (error) {
-    logger.error(error);
-    throw new ApiError(httpStatus.UNAUTHORIZED.code, 'Please authenticate');
-  }
-};
- 
-module.exports = {
-  login,
-  refreshAuthToken,
-};
+    return user;
+  };
+   
+  const refreshAuthToken = async (refreshToken) => {
+    console.log("refreshToken",refreshToken);
+    try {
+      const refreshTokenDoc = await tokenService.verifyToken(
+        refreshToken,
+        tokenTypes.REFRESH
+      );
+      console.log("refreshTokenDoc",refreshTokenDoc);
+      const user = await userService.getUserById(refreshTokenDoc.user);
+      if (!user) {
+        throw new Error();
+      }
+      console.log("refreshTokenDoc=>",refreshTokenDoc);
+      await refreshTokenDoc.deleteOne();
+      return tokenService.generateAuthTokens(user.id);
+    } catch (error) {
+      throw new ApiError(httpStatus.UNAUTHORIZED.code, 'Please authenticate');
+    }
+  };
+   
+  module.exports = {
+    login,
+    refreshAuthToken
+  };
