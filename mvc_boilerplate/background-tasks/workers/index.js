49d3113d368e5743dce1fcebca42740c36dbe7e3
Alex Perea <ing.alexperea@gmail.com>
2024-11-02 12:17:36 -0500

    Added logging to redis workers queue

--- mvc_boilerplate/background-tasks/workers/index.js
+++ mvc_boilerplate/background-tasks/workers/index.js
@@ -1,7 +1,7 @@
 const { Worker } = require('bullmq');
 const path = require('path');
 const config = require('../../config/config');
-
+const logger = require('../../config/logger');
 
 const start = () => {
     const processorPath = path.join(__dirname, 'image-processor.js');
@@ -13,11 +13,15 @@ const start = () => {
                 host: config.redis.host,
                 port: config.redis.port,
             },
-            autorun: true
+            autorun: true,
+            // removeOnComplete: true, // removes once completed, dont include if completed works need to be analyzed later on
+            concurrency: 3 // how many works to process simultaneously
         },
     );
 
-    ImageProcessorWorker.on('completed', (job) => `completed job ${job.id}` );
+    ImageProcessorWorker.on('completed', (job) => 
+        logger.info(`completed job ${job.id}`)
+    );
 }
 
 module.exports = { start };
